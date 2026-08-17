03dfe1aae5fad6adff199a33697f3d8ae87de996
Ahmad Alrashdan <ahmad.mo.alrashdan@gmail.com>
2026-08-17 16:25:09 +0300

    ADD(frontend)/Search Result Card
    Creating the card that will be a template to show the results of searching.

---
+++ frontend/src/components/ResultCard.tsx
@@ -0,0 +1,86 @@
+import '../styles/ResultCard.css';
+
+export default function ResultCard() {
+    return (
+        <>
+        {/* <!-- frame: Result Card - Eng --> */}
+        <div className="frame result-car-7d8d4e22eeda">
+            {/* <!-- group: Group --> */}
+            <div className="group group-7d92fe407326">
+                {/* <!-- group: Group --> */}
+                <div className="group group-7d92e55fdc9f">
+                {/* <!-- rect: Rectangle --> */}
+                <div className="shape rect rectangle-7d8d6659995f">
+                </div>
+                {/* <!-- text: Lg --> */}
+                <div className="shape text lg-7d91bb1c53b9">
+                    <div className="text-node-html" id="html-text-node-63208304-40ae-80e5-8008-7d91bb1c53b9" data-x="1989" data-y="31">
+                    <div className="root rich-text root-0">
+                        <div className="paragraph-set root-0-paragraph-set-0">
+                        <p className="paragraph root-0-paragraph-set-0-paragraph-0" dir="ltr"><span className="text-node root-0-paragraph-set-0-paragraph-0-text-0">Lg</span></p>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                {/* <!-- text: example.com --> */}
+                <div className="shape text exampleco-7d8f27600cba">
+                <div className="text-node-html" id="html-text-node-63208304-40ae-80e5-8008-7d8f27600cba" data-x="2024.9999999999982" data-y="40">
+                    <div className="root rich-text root-0">
+                    <div className="paragraph-set root-0-paragraph-set-0">
+                        <p className="paragraph root-0-paragraph-set-0-paragraph-0" dir="ltr"><span className="text-node root-0-paragraph-set-0-paragraph-0-text-0">example.com</span></p>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                {/* <!-- text: Website Owner --> */}
+                <div className="shape text website-ow-7d8dee3f19c4">
+                <div className="text-node-html" id="html-text-node-63208304-40ae-80e5-8008-7d8dee3f19c4" data-x="2024.9999999999982" data-y="18.999999999999886">
+                    <div className="root rich-text root-0">
+                    <div className="paragraph-set root-0-paragraph-set-0">
+                        <p className="paragraph root-0-paragraph-set-0-paragraph-0" dir="ltr"><span className="text-node root-0-paragraph-set-0-paragraph-0-text-0">Website Owner</span></p>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            {/* <!-- text: Website Name: Website Desc... --> */}
+            <div className="shape text website-na-7d907cc7864b">
+                <div className="text-node-html" id="html-text-node-63208304-40ae-80e5-8008-7d907cc7864b" data-x="1976.9999999999982" data-y="67">
+                <div className="root rich-text root-0">
+                    <div className="paragraph-set root-0-paragraph-set-0">
+                    <p className="paragraph root-0-paragraph-set-0-paragraph-0" dir="ltr"><span className="text-node root-0-paragraph-set-0-paragraph-0-text-0">Website Name: Website Desc...</span></p>
+                    </div>
+                </div>
+                </div>
+            </div>
+            {/* <!-- text: More detailed description about the website and what is the purpose of it. --> */}
+            <div className="shape text more-detai-7d90ec84081e">
+                <div className="text-node-html" id="html-text-node-63208304-40ae-80e5-8008-7d90ec84081e" data-x="1976.9999999999982" data-y="105">
+                <div className="root rich-text root-0">
+                    <div className="paragraph-set root-0-paragraph-set-0">
+                    <p className="paragraph root-0-paragraph-set-0-paragraph-0" dir="ltr"><span className="text-node root-0-paragraph-set-0-paragraph-0-text-0">More detailed description about the website and what is the purpose of it.</span></p>
+                    </div>
+                </div>
+                </div>
+            </div>
+            {/* <!-- group: Group --> */}
+            <div className="group group-7dd367059f5d">
+                {/* <!-- rect: Rectangle --> */}
+                <div className="shape rect rectangle-7dd1f731e9bc">
+                </div>
+                {/* <!-- text: IMG --> */}
+                <div className="shape text i-m-g-7dd33c2ca551">
+                <div className="text-node-html" id="html-text-node-172394bc-fbe4-806d-8008-7dd33c2ca551" data-x="2525.5" data-y="88">
+                    <div className="root rich-text root-0">
+                    <div className="paragraph-set root-0-paragraph-set-0">
+                        <p className="paragraph root-0-paragraph-set-0-paragraph-0" dir="ltr"><span className="text-node root-0-paragraph-set-0-paragraph-0-text-0">IMG</span></p>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
